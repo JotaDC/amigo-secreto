ea11286e73a45e31279125f17a93ef5c44f88620
JotaDC <javierdariocanete@gmail.com>
2025-01-22 20:31:45 -0300

    funcion agregarAmigo con validacion

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+const agregarAmigo = () => {
+    //Capturar el valor del campo de entrada
+    let amigo = document.querySelector('#amigo').value
+    //Validaciones
+    //Validar que el campo no esté vacío
+    if (trim(amigo) === '') {
+        alert('Por favor, inserte un nombre')
+        return
+    } else {
+        //Validar que el campo no sea un número
+        if (!isNaN(amigo)) {
+            alert('Por favor, inserte un nombre')
+            return
+        }
+        else {
+            //Validar que el amigo no se repita
+            if (amigos.includes(amigo)) {
+                alert('El amigo ya fue agregado')
+                return
+            }
+            else {
+                amigos.push(amigo)
+                console.log(amigos)
+                document.querySelector('#amigo').value = ''
+            }
+        }
+    }
+
+}
